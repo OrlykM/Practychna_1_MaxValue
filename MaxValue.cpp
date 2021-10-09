11e6aeed45a301deacd260a7788e53f43feb5569
Maxim <87619450+OrlykM@users.noreply.github.com>
2021-10-09 17:00:22 +0300

    Add files via upload

---
+++ MaxValue.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n, k;
+	cout << "Enter size :>>> ";
+	cin >> n;
+	int* arry = new int[n+1];
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Enter value: ";
+		cin >> k;
+		arry[i] = k;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		if (arry[i] >= arry[i+1] && arry[i] >= arry[0]) {
+			arry[0] = arry[i];
+		}
+	}
+	cout << "Max value is: " << arry[0];
+	delete[] arry;
+}
